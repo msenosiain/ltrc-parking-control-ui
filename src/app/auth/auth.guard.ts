6cbb1ea3fc65b80b4644872eec39d1ccdb7638a9
Senosiain, Miguel <miguel.senosiain@mmc.com>
2025-04-08 11:40:49 -0300

    Remove unwanted code

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,17 +1,13 @@
 import {inject} from '@angular/core';
-import {CanActivateFn, Router} from '@angular/router';
+import {CanActivateFn} from '@angular/router';
 import {AuthService} from './auth.service';
 
 export const authGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
-  const router = inject(Router);
 
-  if (authService.isLoggedIn()) {
-    console.log('logged in');
-    return true;
-  } else {
-    // router.navigate(['/login']);
+  if (!authService.isLoggedIn()) {
     authService.loginWithGoogle();
     return false;
   }
+  return authService.isLoggedIn();
 };
