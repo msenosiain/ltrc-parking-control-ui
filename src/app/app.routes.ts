e0db0b98caea31ae58c1834262e976c41368e3d7
Senosiain, Miguel <miguel.senosiain@mmc.com>
2025-04-08 16:03:09 -0300

    Diaable auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import {Routes} from '@angular/router';
-import {authGuard} from './auth/auth.guard';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {SearchMemberComponent} from './members/components/search-member/search-member.component';
 import {AuthCallbackComponent} from './auth/components/auth-callback/auth-callback.component';
@@ -7,5 +6,5 @@ import {AuthCallbackComponent} from './auth/components/auth-callback/auth-callba
 export const routes: Routes = [
   {path: '', component: SearchMemberComponent},
   {path: 'auth/callback', component: AuthCallbackComponent},
-  {path: 'dashboard', component: DashboardComponent, canActivate: [authGuard]},
+  {path: 'dashboard', component: DashboardComponent},
 ];
