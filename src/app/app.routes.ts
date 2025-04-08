3f2c69e795701d5ccbd9535920df022fbb88d55a
Senosiain, Miguel <miguel.senosiain@mmc.com>
2025-04-08 20:33:55 -0300

    Add auth guard to Dashboard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import {Routes} from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {SearchMemberComponent} from './members/components/search-member/search-member.component';
 import {AuthCallbackComponent} from './auth/components/auth-callback/auth-callback.component';
+import {authGuard} from './auth/auth.guard';
 
 export const routes: Routes = [
   {path: '', component: SearchMemberComponent},
   {path: 'auth/callback', component: AuthCallbackComponent},
-  {path: 'dashboard', component: DashboardComponent},
+  {path: 'dashboard', component: DashboardComponent, canActivate: [authGuard]},
 ];
